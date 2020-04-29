9f8e0a7eb9c292a0765c4daf43bf26e6d2516cd1
JungwooSim <cscd053@gmail.com>
2020-04-29 12:13:44 +0900

    34. 진짜 영속화 / 소스 내용 : h2 databases의 기본 설정인 InMemory → File 구조로 변경 , application.yml 설정을 통해 profile의 변수를 통해 다른 환경 설정 적용

--- eatgo-admin-api/src/main/java/me/restaurant/interfaces/MenuItemController.java
+++ eatgo-admin-api/src/main/java/me/restaurant/interfaces/MenuItemController.java
@@ -3,10 +3,7 @@ package me.restaurant.interfaces;
 import me.restaurant.application.MenuItemService;
 import me.restaurant.domain.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +13,13 @@ public class MenuItemController {
 
     @Autowired
     MenuItemService menuItemService;
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> list(@PathVariable Long restaurantId) {
+        List<MenuItem> menuItems = menuItemService.getMenuItems(restaurantId);
+
+        return menuItems;
+    }
 
     @PatchMapping("/restaurants/{restaurantId}/menuitems")
     public String bulkUpdate(
--- eatgo-admin-api/src/test/java/me/restaurant/interfaces/MenuItemControllerTests.java
+++ eatgo-admin-api/src/test/java/me/restaurant/interfaces/MenuItemControllerTests.java
@@ -1,6 +1,7 @@
 package me.restaurant.interfaces;
 
 import me.restaurant.application.MenuItemService;
+import me.restaurant.domain.MenuItem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -27,6 +35,18 @@ public class MenuItemControllerTests  {
 
     @MockBean
     MenuItemService menuItemService;
+
+    @Test
+    public void list() throws Exception {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(MenuItem.builder().name("Kimchi").build());
+
+        given(menuItemService.getMenuItems(1L)).willReturn(menuItems);
+
+        mvc.perform(get("/restaurants/1/menuitems"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Kimchi")));
+    }
 
     @Test
     public void bulkUpdate() throws Exception {
